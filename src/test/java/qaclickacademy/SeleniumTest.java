1d74c3c92c80dd620cf474b86148902f67fc3417
stephenamala <drop2stephen@gmail.com>
2020-05-01 18:07:54 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	
+	
+	@Test
+	
+	public void browsertesting()
+	{
+		System.out.println("browser");
+	}
+	
+	
+	
+	@Test
+
+	public void elements()
+	{
+		System.out.println("elements");
+	}
+	
+
+}
---
+++ src/test/java/qaclickacademy/demoTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+
+	
+	
+	
+	
+@Test
+	
+	public void shoppingtest()
+	{
+		System.out.println("shop");
+	}
+	
+	
+	
+	@Test
+
+	public void websitetest()
+	{
+		System.out.println("website");
+	}
+	
+}
